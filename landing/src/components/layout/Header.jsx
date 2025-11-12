6c63ea4b39afc9f3f404df492d8da6f8bda4dfa9
medié <danimedie@gmail.com>
2025-11-12 19:03:44 +0100

    setup router

--- landing/src/components/layout/Header.jsx
+++ landing/src/components/layout/Header.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <nav className="flex gap-4">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
         </header>
     );
 }
